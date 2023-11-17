2cce1e2fb52463e955768cd6385fe50f7b98448f
Mustafa Ahmadi <75163126+mustafa-aai@users.noreply.github.com>
2023-11-17 13:25:13 -0800

    added security group

--- main.tf
+++ main.tf
@@ -17,8 +17,25 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
+  vpc_security_group_ids  = [module_web-sg.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
 }
+
+module "security-group-web" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+  name  = "web-sg"
+
+  vpc_id  = data.aws_vpc.default.id
+  
+  ingress_rules  = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules  = ["all-all"]
+  ebsgress_cidr_blocks = ["0.0.0.0/0"]
+
+}
+
+
